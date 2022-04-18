d9d7e5ad1b594be94bd500d3f01f3979d302dc66
dlwjdals <jml22zzang@gmail.com>
2022-04-18 22:25:02 +0900

    Add : feature/wish-list 레이아웃 구현 중

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './pages/Main/Main';
+import WishList from './pages/WishList/WishList';
+
 function App() {
-  return <div className="App" />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/wishlist" element={<WishList />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
